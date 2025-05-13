3c7cc670e5c2e070faac275b07545688b5c13215
Carlos Saire <carlos.saire@tecsup.edu.pe>
2025-05-13 12:12:26 -0500

    Termiando el inputnField

---
+++ Assets/Scripts/Singleton/GameManager.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager instance;
+    [SerializeField] private TMP_Text text;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void InputWep(string value)
+    {
+        value = value.ToLower();
+
+        if (value == "true")
+        {
+            WebGLInput.captureAllKeyboardInput = true;
+        }
+        else if (value == "false")
+        {
+            WebGLInput.captureAllKeyboardInput = false;
+        }
+        else 
+        {
+            Debug.LogWarning("InputWep recibió un valor no válido: " + value);
+        }
+    }
+    public void GetInputFiel(string value)
+    {
+        text.text = value;
+    }
+}
